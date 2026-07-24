562413c8a4c10db8ff2b345f99c12de2e783d2e6
nzach123 <nzach123@live.com>
2026-07-23 21:08:34 -0600

    Add moving hazard scene and script
    Add a new moving hazard scene (res://_res/scenes/hazard_moving.tscn) and script (res://_res/scripts/hazard_moving.gd) that tween-oscillates the hazard's position. Exposes duration and offset as export vars. Scene includes StaticBody2D, CollisionShape2D and a sprite region. Instance of the new scene added to level_02.tscn at position (285, 438). Also add the script UID file.

---
+++ _res/scripts/hazard_moving.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var duration: float = 3.0
+@export var offset: Vector2 = Vector2(200,0.0)
+@onready var hazard_moving: Node2D = $"."
+@onready var static_body_2d: StaticBody2D = $StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	moving_platorm()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func moving_platorm()-> void:
+	var tween: Tween = create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops()
+	tween.set_parallel(false)
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	
+	var half_time: float = duration / 2.0
+	var start_pos = hazard_moving.global_position
+	tween.tween_property(hazard_moving, "position", start_pos + offset, half_time)
+	tween.tween_property(hazard_moving, "position", start_pos, half_time)
+	
+	
+	
